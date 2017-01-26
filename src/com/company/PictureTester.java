d6bea0cfa8d896162b6e7c3c7b82004fd1d44aa0
BlackMaria <sig91308@gmail.com>
2017-01-25 22:05:22 -0500

    Done? I hope now?

--- src/com/company/PictureTester.java
+++ src/com/company/PictureTester.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.awt.*;
+
 /**
  * This class contains class (static) methods
  * that will help you test the Picture class 
@@ -10,107 +12,120 @@ package com.company;
  */
 public class PictureTester
 {
-  /** Method to test zeroBlue */
-  public static void testZeroBlue()
-  {
-    Picture beach = new Picture("beach.jpg");
-    beach.explore();
-    beach.zeroBlue();
-    beach.explore();
-  }
+    /** Method to test zeroBlue */
+    public static void testZeroBlue()
+    {
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        beach.zeroBlue();
+        beach.explore();
+    }
 
-  /** Method to test mirrorVertical */
-  public static void testMirrorVertical()
-  {
-    Picture caterpillar = new Picture("caterpillar.jpg");
-    caterpillar.explore();
-    caterpillar.mirrorVertical();
-    caterpillar.explore();
-  }
+    /** Method to test mirrorVertical */
+    public static void testMirrorVertical()
+    {
+        Picture caterpillar = new Picture("caterpillar.jpg");
+        caterpillar.explore();
+        caterpillar.mirrorVertical();
+        caterpillar.explore();
+    }
 
-  /** Method to test mirrorTemple */
-  public static void testMirrorTemple()
-  {
-    Picture temple = new Picture("temple.jpg");
-    temple.explore();
-    temple.mirrorTemple();
-    temple.explore();
-  }
+    /** Method to test mirrorTemple */
+    public static void testMirrorTemple()
+    {
+        Picture temple = new Picture("temple.jpg");
+        temple.explore();
+        temple.mirrorTemple();
+        temple.explore();
+    }
 
-  /** Method to test the collage method */
-  public static void testCollage()
-  {
-    Picture canvas = new Picture("640x480.jpg");
-    canvas.explore();
-  }
+    /** Method to test the collage method */
+    public static void testCollage()
+    {
+        Picture canvas = new Picture("640x480.jpg");
+        canvas.explore();
+    }
 
-  /** Method to test edgeDetection */
-  public static void testEdgeDetection()
-  {
-    Picture swan = new Picture("swan.jpg");
-    swan.edgeDetection(10);
-    swan.explore();
-  }
+    /** Method to test edgeDetection */
+    public static void testEdgeDetection(int edgeDist)
+    {
+        Picture swan = new Picture("swan.jpg");
+        swan.edgeDetection(10);
+        swan.explore();
+        Pixel leftPixel = null;
+        Pixel rightPixel = null;
+        Pixel [] [] pixels = swan.getPixels2D();
+        Color rightColor = null;
+        for (int row = 0; row < pixels.length; row++) {
+            for (int col = 0; col < pixels[0].length-1; col++) {
+                leftPixel = pixels[row][col];
+                rightPixel = pixels[row][col+1];
+                rightColor = rightPixel.getColor();
+                if (leftPixel.colorDistance(rightColor)>edgeDist)
+                    leftPixel.setColor(Color.black);
+                else
+                    leftPixel.setColor(Color.WHITE);
+            }
+        }
+    }
 
-  /** Main method for testing.  Every class can have a main
-   * method in Java */
-  public static void main(String[] args)
-  {
-    // uncomment a call here to run a test
-    // and comment out the ones you don't want
-    // to run
-    //testZeroBlue();
-    testKeepOnlyBlue();
-    //testKeepOnlyRed();
-    //testKeepOnlyGreen();
-    testNegate();
-    testGreyscale();
-    testFixUnderwater();
-    //testMirrorVertical();
-    //testMirrorTemple();
-    //testMirrorArms();
-    //testMirrorGull();
-    //testMirrorDiagonal();
-    //testCollage();
-    //testCopy();
-    //testEdgeDetection();
-    //testEdgeDetection2();
-    //testChromakey();
-    //testEncodeAndDecode();
-    //testGetCountRedOverValue(250);
-    //testSetRedToHalfValueInTopHalf();
-    //testClearBlueOverValue(200);
-    //testGetAverageForColumn(0);
-  }
+    /** Main method for testing.  Every class can have a main
+     * method in Java */
+    public static void main(String[] args)
+    {
+        // uncomment a call here to run a test
+        // and comment out the ones you don't want
+        // to run
+        //testZeroBlue();
+        //testKeepOnlyBlue();
+        //testKeepOnlyRed();
+        //testKeepOnlyGreen();
+        //testNegate();
+        //testGreyscale();
+        testFixUnderwater();
+        //testMirrorVertical();
+        //testMirrorTemple();
+        //testMirrorArms();
+        //testMirrorGull();
+        //testMirrorDiagonal();
+        //testCollage();
+        //testCopy();
+        testEdgeDetection(1);
+        //testEdgeDetection2();
+        //testChromakey();
+        //testEncodeAndDecode();
+        //testGetCountRedOverValue(250);
+        //testSetRedToHalfValueInTopHalf();
+        //testClearBlueOverValue(200);
+        //testGetAverageForColumn(0);
+    }
 
-  private static void testKeepOnlyBlue() {
-    Picture beach = new Picture("beach.jpg");
-    beach.explore();
-    beach.keepOnlyBlue();
-    beach.explore();
-  }
-  private static void testNegate() {
-    Picture beach = new Picture("beach.jpg");
-    beach.explore();
-    beach.negate();
-    beach.explore();
-  }
+    private static void testFixUnderwater() {
+        Picture beach = new Picture("water.jpg");
+        beach.explore();
+        beach.fixUnderwater();
+        beach.explore();
+    }
 
+    private static void testGreyscale() {
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        beach.greyscale();
+        beach.explore();
+    }
 
-  private static void testGreyscale() {
-    Picture beach = new Picture("beach.jpg");
-    beach.explore();
-    beach.greyscale();
-    beach.explore();
-  }
-
-  private static void testFixUnderwater() {
-    Picture beach = new Picture("water.jpg");
-    beach.explore();
-    beach.fixUnderwater();
-    beach.explore();
-  }
-  //Totes not spam
+    private static void testKeepOnlyBlue() {
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        beach.keepOnlyBlue();
+        beach.explore();
+    }
+    private static void testNegate() {
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        beach.negate();
+        beach.explore();
+    }
 
 
 }
